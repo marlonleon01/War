c3419e17ed9c1ec437971c5986e083dbdf77dd54
Marlon Leon <ghostveene@gmail.com>
2023-02-16 10:14:30 -0500

    drawCardBtn displaying cards

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
 function drawCards() {
     fetch(`${baseUrl}${deckId}/draw/?count=2`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            const cardImgs = document.getElementById("cards-img-container")
+            cardImgs.innerHTML = `
+                                    <img src="${data.cards[0].image}"></img>
+                                    <img src="${data.cards[1].image}"></img>
+                            `
+        })
 }
 
 document.getElementById("draw-cards").addEventListener("click", drawCards)
+
+
